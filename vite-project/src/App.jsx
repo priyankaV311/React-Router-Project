567e413873c917f0d1cbcea5e47f1e0f9bcd3536
priyankaV311 <pv3112003@gmail.com>
2024-09-12 21:10:54 +0530

    you can see the changes in router

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,18 +1,26 @@
 // import React from 'react'
 
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
+// import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
+import { BrowserRouter, Routes, Route, Link, NavLink } from "react-router-dom"
 import Home from './pages/Home/Home'
-import About from './pages/About/About'
+// 'import About from './pages/About/About
 
 
 function App() {
   return (
     
     <BrowserRouter>
+    <header>
+      <nav>
+        <h1>Find jobs?</h1>
+        <Link to='/'></Link>
+      </nav>
+    </header>
     <main>
+
       <Routes>
       <Route path="/" element={<Home/>} />
-      <Route path="about" element={<About/>} />
+      {/* <Route path="about" element={<About/>} /> */}
       </Routes>
     </main>
     </BrowserRouter>
@@ -22,5 +30,6 @@ function App() {
 }
 
 export default App
+
 
 
