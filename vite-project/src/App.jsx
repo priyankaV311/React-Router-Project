15ddbaf3687c81b4d036c2db7591215422ed2cdc
priyankaV311 <pv3112003@gmail.com>
2024-09-13 12:06:10 +0530

    newer way to use react router

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,36 +1,49 @@
-// import React from 'react'
 
+// import { BrowserRouter, Routes, Route, Link, NavLink } from "react-router-dom"
+import { createBrowserRouter, Route, createRoutesFromElements, RouterProvider } from "react-router-dom"
 
-import { BrowserRouter, Routes, Route, Link, NavLink } from "react-router-dom"
 import Home from './pages/Home/Home'
 import About from "./pages/About/About"
 
+import RootLayouts from "./layouts/RootLayouts"
 
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<RootLayouts/>}>
+      <Route path="/" element={<Home/>}></Route>
+      <Route path="about" element={<About/>}></Route>
+
+    </Route>
+
+  )
+)
 
 function App() {
   return (
     
-    <BrowserRouter>
-    <header>
-      <nav>
-        <h1>Find jobs?</h1>
-        <NavLink to='/'>Home</NavLink>
-        <NavLink to="about">About</NavLink>
-      </nav>
-    </header>
-    <main>
+//     <BrowserRouter>
+//     <header>
+//       <nav>
+//         <h1>Find jobs?</h1>
+//         <NavLink to='/'>Home</NavLink>
+//         <NavLink to="about">About</NavLink>
+//       </nav>
+//     </header>
+//     <main>
 
-      <Routes>
-      <Route path="/" element={<Home/>} />
-      <Route path="about" element={<About/>} />
-      </Routes>
-    </main>
-    </BrowserRouter>
+//       <Routes>
+//       <Route path="/" element={<Home/>} />
+//       <Route path="about" element={<About/>} />
+//       </Routes>
+//     </main>
+//     </BrowserRouter>
 
-    
+
+    <RouterProvider router={router}/>
   )
 }
 
 export default App
+
 
 
---
+++ vite-project/src/layouts/RootLayouts.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { NavLink, Outlet } from 'react-router-dom'
+
+function RootLayouts() {
+  return (
+    <div className="root layout">
+        <header>
+            <nav>
+                <h1>Find Jobs ?</h1>
+                <NavLink to='/'>Home</NavLink>
+                <NavLink to="about">About</NavLink>
+            </nav>
+        </header>
+        <main>
+            <Outlet/>
+        </main>
+
+    </div>
+  )
+}
+
+export default RootLayouts
